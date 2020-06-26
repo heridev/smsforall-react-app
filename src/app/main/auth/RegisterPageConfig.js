bbfaefb90b2fcdee0e33f46ae110daa545cb4de1
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-06-25 20:08:16 -0500

    Test the exact as true configuration for routes

--- src/app/main/auth/RegisterPageConfig.js
+++ src/app/main/auth/RegisterPageConfig.js
@@ -1,34 +1,34 @@
 import RegisterPage from './RegisterPage';
 
 const RegisterPageConfig = {
-	settings: {
-		layout: {
-			config: {
-				navbar: {
-					display: false
-				},
-				toolbar: {
-					display: false
-				},
-				footer: {
-					display: false
-				},
-				leftSidePanel: {
-					display: false
-				},
-				rightSidePanel: {
-					display: false
-				}
-			}
-		}
-	},
-	auth: null,
-	routes: [
-		{
-			path: '/auth/register',
-			component: RegisterPage
-		}
-	]
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false
+        },
+        toolbar: {
+          display: false
+        },
+        footer: {
+          display: false
+        },
+        leftSidePanel: {
+          display: false
+        },
+        rightSidePanel: {
+          display: false
+        }
+      }
+    }
+  },
+  auth: null,
+  routes: [
+    {
+      path: '/auth/register',
+      component: RegisterPage
+    }
+  ]
 };
 
 export default RegisterPageConfig;
