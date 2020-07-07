b314961a96b44c7e4c4135978431bf0286f3d2ec
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-07-07 17:03:07 -0500

    Fixes Curl command documentation

--- src/app/main/user/api/CurlCode.js
+++ src/app/main/user/api/CurlCode.js
@@ -11,16 +11,11 @@ const CurlCode = () => {
     <div>
       <FuseHighlight component="pre" className="language-shell my-16">
         {`
-curl -X POST \
-
-  https://api.smsparatodos.com/v2/sms/create \
-
-  -H "Content-Type: application/json" \
-
-  -H 'Authorization-Token: Bearer ${authorizationToken}' \
-
-  -H 'Authorization-Client: ${authorizationClient}' \
-
+curl -X POST \\
+  https://api.smsparatodos.com/v2/sms/create \\
+  -H "Content-Type: application/json" \\
+  -H 'Authorization-Token: Bearer ${authorizationToken}' \\
+  -H 'Authorization-Client: ${authorizationClient}' \\
   -d '{"sms_number":"+523121231517","sms_content":"smsparatodos.com es increíble","sms_type":"standard_delivery","sms_customer_reference_id":"123456789","mobile_hub_id":"6d5be464-xxxx-xxxx-xxxx-90c7242c70d7"}'
 				`}
       </FuseHighlight>
