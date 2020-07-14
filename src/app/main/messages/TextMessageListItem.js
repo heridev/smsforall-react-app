87d2496b3394492afe3395c2bda8d2ce73f4ed12
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2020-07-13 19:17:54 -0500

    Removes unnecesary check for sms notification decorated status
    We removed that line as this could change in the future and we
    will forget about updating this line

--- src/app/main/messages/TextMessageListItem.js
+++ src/app/main/messages/TextMessageListItem.js
@@ -54,8 +54,8 @@ const TextMessageListItem = props => {
   const renderProcessedBy = textMessageAttrs => {
     const processedBy = textMessageAttrs.processed_by_sms_mobile_hub;
     const decoratedStatus = textMessageAttrs.decorated_status;
-    if (decoratedStatus === 'delivered') return null;
-    if (!processedBy.data) return null;
+    // if (decoratedStatus === 'delivered') return null;
+    if (!(processedBy && processedBy.data)) return null;
 
     return `Processed by: ${processedBy.data.attributes.device_name}`;
   };
